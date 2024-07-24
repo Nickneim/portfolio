0a4b15a13a9d9d3cb2832182c93ec309f89a5cb6
Nickneim <nicolasmontes@gmail.com>
2024-07-24 17:12:21 -0300

    update homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,21 +1,23 @@
 import { BlogPosts } from '@/app/components/posts'
-import Description from '@/app/components/description'
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { useTranslations } from 'next-intl';
 
 export default function Page({
   params: {locale}
 } : {
   params: {locale: string};
 }) {
+  unstable_setRequestLocale(locale)
+  const t = useTranslations("HomePage")
 
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 className="mb-8 text-4xl font-semibold tracking-tighter">
+        {t('title')}
       </h1>
-      <Description params={{locale}} />
-      <div className="my-8">
-        <BlogPosts params={{locale}} />
-      </div>
+      <p className="mb-4 text-2xl">
+        {t('about')}
+      </p>
     </section>
   )
 }
