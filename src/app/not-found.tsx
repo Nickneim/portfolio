06cb20fc240a51b0b3659be2f4796000baca1730
Nickneim <nicolasmontes@gmail.com>
2024-07-24 01:49:46 -0300

    fix not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,24 @@
+'use client';
+ 
+import Error from 'next/error';
+ 
 export default function NotFound() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
-    </section>
-  )
+    <html lang="en">
+      <body>
+        <Error statusCode={404} />
+      </body>
+    </html>
+  );
 }
+
+// export default function NotFound() {
+//   return (
+//     <section>
+//       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+//         404 - Page Not Found
+//       </h1>
+//       <p className="mb-4">The page you are looking for does not exist.</p>
+//     </section>
+//   )
+// }
